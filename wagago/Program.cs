34a3ec30e263030ad3a26d10563e29cc358e3a25
gnerkus <ifeanyioraelosi@gmail.com>
2024-05-08 21:23:17 +0200

    scaffold main class and Scanner and Token classes

--- wagago/Program.cs
+++ wagago/Program.cs
@@ -1,3 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+namespace Wagago
+{
+    public class Wagago
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: wagago [script]");
+                Environment.Exit(1);
+            } else if (args.Length == 1)
+            {
+                runFile(args[0]);
+            } else
+            {
+                runPrompt();
+            }
+        }
 
-Console.WriteLine("Hello, World!");
+        private static void runFile(string filePath)
+        {
+            var contents = File.ReadAllText(filePath);
+            run(contents);
+        }
+
+        private static void runPrompt()
+        {
+            try
+            {
+                Console.WriteLine("> ");
+                while (Console.ReadLine() is { } line)
+                {
+                    run(line);
+                    Console.WriteLine("> ");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("{0}: The read operation could not be performed due to the " + 
+                "error", e.GetType().Name);
+            }
+            
+            
+        }
+
+        private static void run(string source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.scanTokens();
+
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+    }
+
+    public class Scanner
+    {
+        public Scanner(string source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Token> scanTokens()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Token
+    {
+        
+    }
+}
