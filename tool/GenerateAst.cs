68c1e0861b928ea4d02794f9f9ac29b710442496
gnerkus <ifeanyioraelosi@gmail.com>
2024-05-28 13:33:36 +0200

    add explicit access modifiers to generated classes

--- tool/GenerateAst.cs
+++ tool/GenerateAst.cs
@@ -16,11 +16,11 @@
                 case 1:
                     var outputDir = args[0];
                     DefineAst(outputDir, "Expr",
-                        new List<string>()
+                        new List<string>
                         {
                             "Binary   : Expr left, Token operatr, Expr right",
                             "Grouping : Expr expression",
-                            "Literal  : Object value",
+                            "Literal  : object value",
                             "Unary    : Token operatr, Expr right"
                         });
                     break;
@@ -38,49 +38,77 @@
             using var streamWriter = new StreamWriter(path);
             streamWriter.WriteLine("namespace Wagago");
             streamWriter.WriteLine("{");
-            streamWriter.WriteLine($"  abstract class {baseName}");
+            streamWriter.WriteLine($"  internal abstract class {baseName}");
             streamWriter.WriteLine("  {");
+            streamWriter.WriteLine("    public abstract TR Accept<TR>(IVisitor<TR> visitor);");
             streamWriter.WriteLine("  }");
-            
+
+            DefineVisitor(streamWriter, baseName, types);
+
+            // The AST classes
             foreach (var type in types)
             {
                 var className = type.Split(":")[0].Trim();
                 var fields = type.Split(":")[1].Trim();
                 DefineType(streamWriter, baseName, className, fields);
             }
-            
+
             // closing bracket for namespace
             streamWriter.WriteLine("}");
             streamWriter.Close();
         }
 
-        private static void DefineType(StreamWriter writer, string baseName, string className, 
-        string fieldList)
+        /// <summary>
+        ///     generate the visitor interface
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="baseName"></param>
+        /// <param name="types"></param>
+        private static void DefineVisitor(StreamWriter writer, string baseName, List<string> types)
+        {
+            writer.WriteLine(" internal interface IVisitor<out TR> {");
+
+            foreach (var typeName in types.Select(type => type.Split(":")[0].Trim()))
+                writer.WriteLine(
+                    $"    TR Visit{typeName}{baseName}({typeName} {baseName.ToLower()});");
+
+            writer.WriteLine("  }");
+        }
+
+        private static void DefineType(StreamWriter writer, string baseName, string className,
+            string fieldList)
         {
             writer.WriteLine($" class {className}: {baseName}");
             writer.WriteLine("  {");
-            
+
             // Constructor
-            writer.WriteLine($"    {className}({fieldList})");
+            writer.WriteLine($"    private {className}({fieldList})");
             writer.WriteLine("    {");
-            
+
             // -- Store parameters in fields
             var fields = fieldList.Split(", ");
             foreach (var field in fields)
             {
                 var name = field.Split(" ")[1];
-                writer.WriteLine($"      this.{name} = {name};");
+                writer.WriteLine($"      _{name} = {name};");
             }
-            
+
             writer.WriteLine("    }");
             
+            writer.WriteLine();
+            writer.WriteLine("    public override TR Accept<TR>(IVisitor<TR> visitor)");
+            writer.WriteLine("    {");
+            writer.WriteLine($"      return visitor.Visit{className}{baseName}(this);");
+            writer.WriteLine("    }");
+
             // Fields
             writer.WriteLine();
             foreach (var field in fields)
             {
-                writer.WriteLine($"    readonly {field};");
+                writer.WriteLine($"    private readonly {field.Replace(" ", " _")};");
             }
-            
+
+
             writer.WriteLine("  }");
         }
     }
