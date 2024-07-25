523cbe7c436e649d8b38ee850fc75380f1e45393
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-07-25 19:03:06 +0200

    define class to represent program global context

---
+++ wagago/Env.cs
@@ -0,0 +1,31 @@
+﻿namespace Wagago
+{
+    public class Env
+    {
+        private readonly Dictionary<string, object> _values = new ();
+
+        public void Define(string name, object value)
+        {
+            _values.Add(name, value);
+        }
+
+        /// <summary>
+        /// Fetch a variable's value by name
+        /// <para>A missing variable is a runtime (not syntax) error
+        /// as part of Wagago's design
+        /// </para>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        /// <exception cref="RuntimeError"></exception>
+        public object Get(Token name)
+        {
+            if (_values.ContainsKey(name.lexeme))
+            {
+                return _values[name.lexeme];
+            }
+
+            throw new RuntimeError(name, $"Undefined variable '{name.lexeme}'.");
+        }
+    }
+}
