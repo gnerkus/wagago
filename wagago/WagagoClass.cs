7783da933ced8d009636364f39e34db9cfaacf6f
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-08-11 14:26:16 +0200

    scaffold definition for class objects in custom interpreter

---
+++ wagago/WagagoClass.cs
@@ -0,0 +1,17 @@
+﻿namespace Wagago
+{
+    public class WagagoClass
+    {
+        readonly string _name;
+
+        public WagagoClass(string name)
+        {
+            _name = name;
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
